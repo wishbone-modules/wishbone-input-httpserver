a053c4f533a231c3df06ca0216d1eb339b5b00b7
Jelle Smet <development@smetj.net>
2016-05-27 19:52:02 +0200

    - Add useful meta data to each event - Update docstring

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools.command.test import test as TestCommand
 import sys
 
 PROJECT = 'wishbone_input_httpserver'
-VERSION = '1.0.0'
+VERSION = '1.0.2'
 
 install_requires = [
     'wishbone>=2.1.1',
